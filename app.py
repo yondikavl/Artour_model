95bd123e5d1a80ea2ae736d5c48824294a92988e
Yondika Vio Landa <105154819+yondikavl@users.noreply.github.com>
2025-07-14 10:32:34 +0700

    update for deploy railway

--- app.py
+++ app.py
@@ -26,5 +26,6 @@ def predict():
     return jsonify({"prediction": prediction})
 
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5000)
+
 
